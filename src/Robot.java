16264b8d1b850449117e02a4c5189dbea7ec64e4
Karl Wolf <wolfkarl@gmail.com>
2013-06-16 21:41:10 +0200

    Gegebene Dateien vom Lehrstuhl

---
+++ src/Robot.java
@@ -0,0 +1,127 @@
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hpi.sam.ordermanagement.Cart;
+import de.hpi.sam.ordermanagement.CartArea;
+import de.hpi.sam.ordermanagement.CartPosition;
+import de.hpi.sam.ordermanagement.IssuingPoint;
+import de.hpi.sam.ordermanagement.Order;
+import de.hpi.sam.ordermanagement.OrderItem;
+import de.hpi.sam.ordermanagement.OrderManagement;
+import de.hpi.sam.robotino.IRobot;
+import de.hpi.sam.robotino.Position;
+import de.hpi.sam.robotino.actor.RobotOmniDrive;
+import de.hpi.sam.robotino.logic.MoveLogic;
+import de.hpi.sam.robotino.sensor.RobotBumper;
+import de.hpi.sam.robotino.sensor.RobotCom;
+import de.hpi.sam.robotino.sensor.RobotDistanceSensorArray;
+import de.hpi.sam.robotino.sensor.RobotNorthStar;
+import de.hpi.sam.robotino.sensor.RobotPowerManagement;
+
+public class Robot implements Runnable, IRobot
+{
+	public OrderManagement management;
+    protected final String hostname;
+    protected final RobotCom com;
+    protected final RobotOmniDrive omniDrive;
+    protected final RobotBumper bumper;
+    protected final RobotNorthStar northStar;
+    protected final MoveLogic moveLogic;
+    protected final RobotDistanceSensorArray distanceSensorArray;
+    protected final RobotPowerManagement powerManagement;
+    
+    protected final double energy;
+    protected final double throughput;
+
+    public Robot(String hostname, OrderManagement management, double energy, double throughput)
+    {
+    	System.out.println("Initializing...");
+    	this.management = management;
+        this.hostname = hostname;
+        com = new RobotCom();
+        com.setAddress(hostname);
+        omniDrive = new RobotOmniDrive(com);
+        bumper = new RobotBumper(com);
+        distanceSensorArray = new RobotDistanceSensorArray(com);
+        northStar = new RobotNorthStar(com);
+        moveLogic = new MoveLogic(omniDrive, northStar, distanceSensorArray);
+        powerManagement = new RobotPowerManagement(com);
+        
+        this.energy = energy;
+        this.throughput = throughput;
+    }
+
+    public void run()
+    {
+        System.out.println("Robot started.");
+        
+        try
+        {
+            System.out.println("Connecting...");
+            com.connect();
+            System.out.println("Connected.");
+            System.out.println("Driving...");
+            drive();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            disconnect();
+        }
+
+        System.out.println("Done.");
+    }
+
+    protected void disconnect()
+    {
+        com.disconnect();
+    }
+
+    protected void drive() throws InterruptedException
+    {
+       
+        while (!Thread.interrupted() && com.isConnected() && false == bumper.value())
+        {
+			//TODO implement this method
+           
+            
+        	moveLogic.setWaipoint(10 * 1000, -10 * 1000);
+        	System.out.println("reached target: "+ moveLogic.transportToCoordinates());
+           
+            com.waitForUpdate();
+           
+            System.out.println("DONE");
+        }
+    }
+
+    /**
+     * Rotate a 2 dimensional vector
+     * 
+     * @param in Input vector
+     * @param deg Rotation in degrees
+     * @return Output vector
+     */
+    private float[] rotate(float[] in, float deg)
+    {
+        final float pi = 3.14159265358979f;
+
+        float rad = 2 * pi / 360.0f * deg;
+
+        float[] out = new float[2];
+        out[0] = (float) (cos(rad) * in[0] - sin(rad) * in[1]);
+        out[1] = (float) (sin(rad) * in[0] + cos(rad) * in[1]);
+        return out;
+    }
+
+	@Override
+	public Position position() {
+		return new Position (this.northStar.posX()/1000, this.northStar.posZ()/1000);
+	}
+}
---
+++ src/Simulation.java
@@ -0,0 +1,34 @@
+import de.hpi.sam.ordermanagement.OrderManagement;
+
+public class Simulation
+{
+
+	public static void main(String[] args)
+	{
+		OrderManagement management = new OrderManagement();
+		
+		/*
+		 * Throughput vs. energy 
+		 * 
+		 * For simulation purposes, we set these values in this main-method. 
+		 * Throughput + Energy is always 1.0 (100%).
+		 */
+		
+		final double energy = 0.3;
+		final double throughput = 0.7;
+		
+		Robot robot1 = new Robot("127.0.0.1:8080", management, energy, throughput);
+		Robot robot2 = new Robot("127.0.0.1:8082", management, energy, throughput);
+
+		Thread thread1 = new Thread(robot1);
+		Thread thread2 = new Thread(robot2);
+
+		management.addRobot(robot1);
+		management.addRobot(robot2);
+
+		System.out.println("Start simulation of robot 1:");
+		thread1.start();
+		System.out.println("Start simulation of robot 2:");
+		thread2.start();
+	}
+}
